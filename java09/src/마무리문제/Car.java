07baaa06abe572a7860b70f80cf3b0eb00061dbe
ChoiHyunSuk93 <121939797+ChoiHyunSuk93@users.noreply.github.com>
2023-02-16 17:51:52 +0900

    Delete java09 directory

--- java09/src/마무리문제/Car.java
+++
@@ -1,9 +0,0 @@
-package 마무리문제;
-
-public class Car {
-	int wheel = 4;
-	
-	public void drive() {
-		System.out.println("도로를 달립니다.");
-	}
-}
--- java09/src/상속/우먼.java
+++
@@ -1,9 +0,0 @@
-package 상속;
-
-public class 우먼 extends 사람 {
-	int brain;
-	
-	public void think() {
-		System.out.println("생각하다.");
-	}
-}
